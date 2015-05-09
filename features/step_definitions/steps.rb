dfe7f7449b96d9f6b894f1ae7f31ba08c8aa8a1c
Kevin Goslar <kevin.goslar@originate.com>
2015-05-08 17:31:58 -0700

    Documentation tables

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -13,3 +13,15 @@ end
 Then(/^it responds with (.+?)$/) do |code|
   @result = eval "@#{code}"
 end
+
+
+Then(/^I have these ingredients$/) do |table|
+  t = Mortadella.new headers: ['INGREDIENT', 'AMOUNT']
+  t << ['flour', '12 oz']
+  t << ['butter', '2 oz']
+  t << ['banana', '2 pc']
+  t << ['apples', '5 pc']
+  t << ['cinnamon', '1 tsp']
+  table.diff! t.table
+end
+
---
+++ features/test.feature
@@ -0,0 +1,10 @@
+Feature: purchasing cookie ingredients
+
+  Scenario: purchasing cookie ingredients
+    Then I have these ingredients
+      | INGREDIENT | AMOUNT |
+      | flour      | 12 oz  |
+      | butter     | 2 oz   |
+      | banana     | 2 pc   |
+      | apples     | 3 pc   |
+      | cinnamon   | 1 tsp  |
