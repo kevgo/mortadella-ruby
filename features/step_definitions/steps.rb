2afe8a8f71c34372758eae592ed70c7fb8cc9e7e
Kevin Goslar <kevin.goslar@gmail.com>
2015-06-12 08:55:43 -0700

    Fix lint errors

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -16,11 +16,10 @@ end
 
 
 Then(/^I have these ingredients$/) do |table|
-  t = Mortadella.new headers: ['INGREDIENT', 'AMOUNT']
+  t = Mortadella.new headers: %w(INGREDIENT AMOUNT)
   t << ['flour', '12 oz']
   t << ['butter', '2 oz']
   t << ['apples', '5 pc']
   t << ['cinnamon', '1 tsp']
   table.diff! t.table
 end
-
