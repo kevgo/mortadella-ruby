a52b137220bd575f6dc8ec6331927e78cbb7d8c9
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-19 15:45:58 -0500

    Update code coverage (#16)

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-# if ENV['CI']
-#   require 'coveralls'
-#   Coveralls.wear!
-# end
+if ENV["CI"]
+  require "simplecov"
+  SimpleCov.start
+end
 
 require "bundler/setup"
 Bundler.setup
