486b13479105239c83f4e00fdaa49118a775d7a0
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-19 15:39:54 -0500

    set up GitHub Actions as CI (#14)

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-if ENV['CI']
-  require 'coveralls'
-  Coveralls.wear!
-end
+# if ENV['CI']
+#   require 'coveralls'
+#   Coveralls.wear!
+# end
 
-require 'bundler/setup'
+require "bundler/setup"
 Bundler.setup
-require 'mortadella'
+require "mortadella"
