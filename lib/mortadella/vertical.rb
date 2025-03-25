4a97a49a1f3cbe9a2aec860352b37dcc959b485f
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-25 09:42:04 -0500

    mortadella.rb horizontal.rb vertical.rb

--- lib/mortadella/vertical.rb
+++ lib/mortadella/vertical.rb
@@ -1,28 +1,21 @@
 # frozen_string_literal: true
 
 module Mortadella
-
   # Makes it easy to build DRY vertical Cucumber-compatible tables
   class Vertical
-
     attr_reader :table
-
 
     def initialize
       @table = []
     end
 
-
     # Adds the given row to the table
-    def []= header, row
+    def []=(header, row)
       @table << [header, row]
     end
-
 
     def empty?
       @table.empty?
     end
-
   end
-
 end
