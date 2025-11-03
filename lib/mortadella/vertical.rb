3b9d494d8d52252950e251daaa9b56ba52befed7
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-03 09:34:07 -0600

    Add YARD type signatures (#33)

--- lib/mortadella/vertical.rb
+++ lib/mortadella/vertical.rb
@@ -3,17 +3,23 @@
 module Mortadella
   # Makes it easy to build DRY vertical Cucumber-compatible tables.
   class Vertical
+    # @return [Array<(String, Array<String>)>] The table as an array of [header, row] pairs.
     attr_reader :table
 
+    # @return [void]
     def initialize
       @table = []
     end
 
     # Adds the given row to the table.
+    # @param header [String] The header for the row.
+    # @param row [String] The data field for the given header.
+    # @return [void]
     def []=(header, row)
       @table << [header, row]
     end
 
+    # @return [Boolean] Indicates whether this table is empty.
     def empty?
       @table.empty?
     end
